4bde2537401ce269a8288be5505e13a54ae07904
Siddharth Majhi <majhysiddharth17121996@gmail.com>
2022-06-26 13:31:55 +0530

    Jwt initial implementation without database

---
+++ src/main/java/com/jwt_poc/model/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.jwt_poc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequest {
+
+    private String username;
+    private String password;
+}
